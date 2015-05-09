db1ea4a9482af68ffda91bcc18273ef70c8ea7e3
William Wnekowicz <william.wnekowicz@gmail.com>
2015-05-09 16:28:15 -0700

    Add RESTful structure for list

--- routes/lists.js
+++ routes/lists.js
@@ -6,7 +6,24 @@ router.route('/')
     next();
   })
   .get(function(req, res, next) {
-    res.send('OK');
+    next(new Error('not implemented'));
+  })
+  .put(function(req, res, next) {
+    next(new Error('not implemented'));
+  })
+  .post(function(req, res, next) {
+    next(new Error('not implemented'));
+  })
+  .delete(function(req, res, next) {
+    next(new Error('not implemented'));
+  });
+
+router.route('/:id')
+  .all(function(req, res, next) {
+    next();
+  })
+  .get(function(req, res, next) {
+    next(new Error('not implemented'));
   })
   .put(function(req, res, next) {
     next(new Error('not implemented'));
