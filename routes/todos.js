b08829b32fa909eaf8b29fe8229b6d4383e3a846
William Wnekowicz <william.wnekowicz@gmail.com>
2015-05-09 19:03:52 -0700

    Add RESTful structure for todos/:id

--- routes/todos.js
+++ routes/todos.js
@@ -22,4 +22,21 @@ router.route('/')
     next(new Error('not implemented'));
   });
 
+router.route('/:id')
+  .all(function(req, res, next) {
+    next();
+  })
+  .get(function(req, res, next) {
+    next(new Error('not implemented'));
+  })
+  .put(function(req, res, next) {
+    next(new Error('not implemented'));
+  })
+  .post(urlencode, function(req, res, next) {
+    next(new Error('not implemented'));
+  })
+  .delete(function(req, res, next) {
+    next(new Error('not implemented'));
+  });
+
 module.exports = router;
