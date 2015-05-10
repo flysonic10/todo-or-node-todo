a5264f43316340ef29a33611d9c4260eb1d1cde1
William Wnekowicz <william.wnekowicz@gmail.com>
2015-05-09 20:22:24 -0700

    Adds GET /lists/:id/todos

--- routes/todos.js
+++ routes/todos.js
@@ -1,16 +1,33 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router({mergeParams: true});
+var List = require('../models/list');
 var Todo = require('../models/todo');
 
 var bodyParser = require('body-parser');
 var urlencode = bodyParser.urlencoded({ extended: false });
+var async = require('async');
 
 router.route('/')
   .all(function(req, res, next) {
     next();
   })
   .get(function(req, res, next) {
-    next(new Error('not implemented'));
+    List.read(req.params.id, function (err, data) {
+      if(err){ res.sendStatus(404); return; }
+
+      var listTodos = [];
+      async.each(data.todos, function (todo, callback) {
+        Todo.read(todo, function (err, data) {
+          if(err){ return next(err); }
+          console.log(data);
+          listTodos.push(data);
+          callback();
+        });
+      }, function(err){
+        if(err){ res.sendStatus(404); return; }
+        res.status(200).json({todos: listTodos});
+      });
+    });
   })
   .put(function(req, res, next) {
     next(new Error('not implemented'));
@@ -27,7 +44,8 @@ router.route('/:id')
     next();
   })
   .get(function(req, res, next) {
-    next(new Error('not implemented'));
+    console.log(req);
+    // next(new Error('not implemented'));
   })
   .put(function(req, res, next) {
     next(new Error('not implemented'));
