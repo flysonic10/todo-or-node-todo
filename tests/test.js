8531318bf1e7bc77e98b006677f7ef770d3c8344
William Wnekowicz <william.wnekowicz@gmail.com>
2015-05-09 20:34:03 -0700

    Clean up tests

--- tests/test.js
+++ tests/test.js
@@ -13,17 +13,18 @@ describe('GET /', function () {
   });
 });
 
-describe('GET /lists', function () {
-  it('Should return a 200 status', function (done) {
-    request(app)
-      .get('/')
-      .expect(200)
-      .end(function (error) {
-        if(error) throw error;
-        done();
-      });
+describe('lists', function () {
+  describe('GET /lists', function () {
+    it('Should return a 200 status', function (done) {
+      request(app)
+        .get('/')
+        .expect(200)
+        .end(function (error) {
+          if(error) throw error;
+          done();
+        });
+    });
   });
-
   describe('POST /lists', function () {
     it('Should return id', function (done) {
       request(app)
@@ -31,6 +32,24 @@ describe('GET /lists', function () {
         .send('name=A New List')
         .expect(/id/i, done);
     });
+  });
+});
 
+
+describe('todos', function () {
+  describe('GET /lists/:id/todos', function () {
+    it('Should return todos');
+  });
+
+  describe('POST /lists/:id/todos', function () {
+    it('Should return id');
+  });
+
+  describe('PUT /lists/:id/todos/:id', function () {
+    it('Should return a 200 status');
+  });
+
+  describe('DELETE /lists/:id/todos/:id', function () {
+    it('Should return a 200 status');
   });
 });
