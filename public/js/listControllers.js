1778d2bf68f3ccd1b9a4207bc556e2713b6d3a34
William Wnekowicz <william.wnekowicz@gmail.com>
2015-05-11 15:07:41 -0700

    Save new lists to close #1 and #4

--- public/js/listControllers.js
+++ public/js/listControllers.js
@@ -3,9 +3,17 @@
 angular.module('ListControllers', ['ngRoute'])
 
 .controller('ListsCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+
   $http.get('api/lists').success(function(data) {
     $scope.lists = data;
   });
+
+  $scope.addItem = function(){
+    $http.post('api/lists', "name="+$scope.listName).success(function(data) {
+      $scope.lists.push({'id': data.id, 'name': $scope.listName});
+    });
+  };
 }])
 
 .controller('ListDetailsCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
