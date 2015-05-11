feb6f00615c8d7bb6f1f11e80f55e99d1700929a
William Wnekowicz <william.wnekowicz@gmail.com>
2015-05-11 16:43:03 -0700

    Add todo checking and unchecked to close #3

--- public/js/todoControllers.js
+++ public/js/todoControllers.js
@@ -3,7 +3,15 @@
 angular.module('TodoControllers', ['ngRoute'])
 
 .controller('TodosCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+  $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+  $http.defaults.headers.put["Content-Type"] = "application/x-www-form-urlencoded";
+
   $http.get('api/lists/'+$routeParams.listId+'/todos').success(function(data) {
     $scope.todos = data.todos;
   });
+  
+  $scope.markStatus = function(todoId, todoChecked){
+    $http.put('api/lists/'+$routeParams.listId+'/todos/'+todoId,
+      "checked="+todoChecked).success(function(data) {});
+  };
 }]);
