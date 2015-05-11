0641d2e6bcefba05e0782f35b45392e1027a1782
William Wnekowicz <william.wnekowicz@gmail.com>
2015-05-10 21:26:51 -0700

    Define routes for web app

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var app = express();
 
+
+/**
+ * Web App Routes
+ */
 app.use(express.static(__dirname + '/public'));
 
+app.get('/lists', function (req, res) {
+  res.redirect('/');
+});
+
+/**
+ * API Routes
+ */
 var lists = require('./routes/lists');
 app.use('/api/lists', lists);
 
 var todos = require('./routes/todos');
 app.use('/api/lists/:list_id/todos', todos);
 
-app.use('/*', function(req, res){
-  res.sendfile(__dirname + '/public/index.html');
-});
-
+/**
+ * Catch-all Route
+ */
 app.get('*', function(req, res){
   res.sendStatus(404);
 });
